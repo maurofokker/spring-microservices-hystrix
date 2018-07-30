ad9e77ee5a62e977beb413ba093b002189623189
Mauricio <mgaldamesc.azurian@bancochile.cl>
2018-07-30 10:53:15 -0400

    add simple circuit breaker with hystrix to a default fallback method

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 * and [simple microservice](https://github.com/maurofokker/spring-microservices-simple-service)
 
 * Create a simple rest to consume simple microservice described above
+
+* In case a target service present a failure, hystrix provide a circuit breaker feature
+* Failures in a microservice architecture can cascade and cause bigger problems
+* Main feature of `Hystrix` is being able to provide a graceful fail over within a micro service architecture
+* If service fail then a default value can be provide to the source... this is provide by `@EnableCircuitBreaker` annotation
+  * this allow to route our execution to a different method in the case of a failure using the annotation method `@HystrinxCommand`
+    ```java
+      @RequestMapping("/simpleServiceClient")
+      @HystrixCommand(fallbackMethod = "failover")
+      public List<String> simpleServiceClient() {
+        return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
+      }
+    
+      public List<String> failover() {
+        return Arrays.asList("default_1", "default_2");    
+      }
+    ```
+    * `fallbackMethod` allow to specify a particular method to invoke in case client method call fails
+    * when `http://localhost:8888/service` fails then `failover()` method will be called
--- src/main/java/com/maurofokker/poc/cloud/SpringMicroservicesHystrixApplication.java
+++ src/main/java/com/maurofokker/poc/cloud/SpringMicroservicesHystrixApplication.java
@@ -1,17 +1,21 @@
 package com.maurofokker.poc.cloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@EnableCircuitBreaker
 public class SpringMicroservicesHystrixApplication {
 
     @Bean
@@ -23,8 +27,13 @@ public class SpringMicroservicesHystrixApplication {
     private RestTemplate restTemplate;
 
     @RequestMapping("/simpleServiceClient")
+    @HystrixCommand(fallbackMethod = "failover")
     public List<String> simpleServiceClient() {
         return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
+    }
+
+    public List<String> failover() {
+        return Arrays.asList("default_1", "default_2");
     }
 
 	public static void main(String[] args) {
